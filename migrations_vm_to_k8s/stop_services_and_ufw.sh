6c2ddfd504b46cb79de7bc73403c3dbf10fa75dd
nicolas <7702787+nicolas-san@users.noreply.github.com>
2020-11-27 18:24:48 +0100

    - first commit

---
+++ migrations_vm_to_k8s/stop_services_and_ufw.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+if [ $(id -u) != 0 ]
+then
+        echo "Needs to be root!"
+        exit
+fi
+
+if [ -z "$1" ]
+then
+        echo "You have to pass the old IP as first parameter"
+        exit
+fi
+
+# stop services
+echo "#### Stopping nginx";
+service nginx stop
+#service nginx status | grep "Active"
+systemctl is-active nginx.service
+
+echo "#### Stopping sendMail";
+service sendMail stop
+#service sendMail status | grep "Active"
+systemctl is-active sendMail.service
+
+echo "#### Stopping sendMailGroup";
+service sendMailGroup stop
+#service sendMailGroup status | grep "Active"
+systemctl is-active sendMailGroup.service
+
+# unregister from startup
+systemctl disable nginx.service
+systemctl is-enabled nginx.service
+
+systemctl disable sendMail.service
+systemctl is-enabled sendMail.service
+
+systemctl disable sendMailGroup.service
+systemctl is-enabled sendMailGroup.service
+
+# empty crontab
+echo "#### Jamespot user crontab:"
+sudo -u jamespot crontab -l
+echo "#### Emptying Jamespot user crontab ..."
+sudo -u jamespot crontab -r
+echo "#### Jamespot user crontab after:"
+sudo -u jamespot crontab -l
+
+# ufw setup ipv4 redirect
+echo "#### Allow UFW to FORWARD by default"
+sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
+
+# https://www.cyberciti.biz/faq/how-to-configure-ufw-to-forward-port-80443-to-internal-server-hosted-on-lan/
+# replace also the COMMIT in the comment: # don't delete the 'COMMIT' line or these rules won't be processed
+echo "#### Change UFW prerouting rules"
+sed -i 's/COMMIT//' /etc/ufw/before.rules
+echo "*nat
+:PREROUTING ACCEPT [0:0]
+# forward vm IP port 80 to 54.38.100.208:80
+# forward vm IP  port 443 to 54.38.100.208:443
+-A PREROUTING -i eth0 -d $1 -p tcp --dport 80 -j  DNAT --to-destination 54.38.100.208:80
+-A PREROUTING -i eth0 -d $1 -p tcp --dport 443 -j  DNAT --to-destination 54.38.100.208:443
+# setup routing
+-A POSTROUTING -s 54.38.100.208/24 ! -d 54.38.100.208/24 -j MASQUERADE
+COMMIT" >> /etc/ufw/before.rules
+
+
+echo "#### Configure sysctl to allow ipV4 forward"
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+
+echo "#### Read the sysctl file"
+sysctl -p
+
+echo "#### Restart UFW"
+service restart ufw
+echo "#### UFW status"
+systemctl status ufw.service
+
+echo "#### IPtables rules"
+iptables -t nat -L -n -v
+
+cat <<EOF
+
+ ▄▄▄██▀▀▀▄▄▄       ███▄ ▄███▓▓█████   ██████  ██▓███   ▒█████  ▄▄▄█████▓
+   ▒██  ▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒ ▓██░  ██▒▒██▒  ██▒▓  ██▒ ▓▒
+   ░██  ▒██  ▀█▄  ▓██    ▓██░▒███   ░ ▓██▄   ▓██░ ██▓▒▒██░  ██▒▒ ▓██░ ▒░
+▓██▄██▓ ░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄   ▒   ██▒▒██▄█▓▒ ▒▒██   ██░░ ▓██▓ ░
+ ▓███▒   ▓█   ▓██▒▒██▒   ░██▒░▒████▒▒██████▒▒▒██▒ ░  ░░ ████▓▒░  ▒██▒ ░
+ ▒▓▒▒░   ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▒░▒░   ▒ ░░
+ ▒ ░▒░    ▒   ▒▒ ░░  ░      ░ ░ ░  ░░ ░▒  ░ ░░▒ ░       ░ ▒ ▒░     ░
+ ░ ░ ░    ░   ▒   ░      ░      ░   ░  ░  ░  ░░       ░ ░ ░ ▒    ░
+ ░   ░        ░  ░       ░      ░  ░      ░               ░ ░
+
+ ██▓███   ▒█████   █     █░▓█████  ██▀███
+▓██░  ██▒▒██▒  ██▒▓█░ █ ░█░▓█   ▀ ▓██ ▒ ██▒
+▓██░ ██▓▒▒██░  ██▒▒█░ █ ░█ ▒███   ▓██ ░▄█ ▒
+▒██▄█▓▒ ▒▒██   ██░░█░ █ ░█ ▒▓█  ▄ ▒██▀▀█▄
+▒██▒ ░  ░░ ████▓▒░░░██▒██▓ ░▒████▒░██▓ ▒██▒
+▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▓░▒ ▒  ░░ ▒░ ░░ ▒▓ ░▒▓░
+░▒ ░       ░ ▒ ▒░   ▒ ░ ░   ░ ░  ░  ░▒ ░ ▒░
+░░       ░ ░ ░ ▒    ░   ░     ░     ░░   ░
+             ░ ░      ░       ░  ░   ░
+
+EOF
+cat << "EOF"
+       _,.
+     ,` -.)
+    '( _/'-\\-.
+   /,|`--._,-^|            ,
+   \_| |`-._/||          ,'|
+     |  `-, / |         /  /
+     |     || |        /  /
+      `r-._||/   __   /  /
+  __,-<_     )`-/  `./  /
+ '  \   `---'   \   /  /
+     |           |./  /
+     /           //  /
+ \_/' \         |/  /
+  |    |   _,^-'/  /
+  |    , ``  (\/  /_
+   \,.->._    \X-=/^
+   (  /   `-._//^`
+    `Y-.____(__}
+     |     {__)
+           ()`
+EOF
+
+echo "Thank you for your service master, you are amazing !"
